113e357cc609454cff593f4e00b2d50661e3bc30
franzmiguel <franzsalinas22@gmail.com>
2025-10-31 07:22:58 -0300

    Enhance product handling and total calculation logic
    Refactor script.js to include product rendering and total calculation.

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
+
     const gridContainer = document.querySelector('.grid-container');
     const totalQuantityDisplay = document.getElementById('total-quantity');
+    const totalAmountDisplay = document.getElementById('total-amount');
+    const templateProd = document.getElementById('productCard').content;
+    const fragment = document.createDocumentFragment();
+    const items = document.querySelector('.grid-container');
+
+const paContinuar = data => {
+        const allInputs = document.querySelectorAll('input');
+        const inputsGreaterThanZero = [];
+        let total = 0;
+        for (let i = 0; i < allInputs.length; i++) {
+            const input = allInputs[i];
+            // Verifica que el input tenga un valor numérico y sea mayor a 0
+            if (input.type === 'text' && parseFloat(input.value) > 0) {
+                amount = Number(input.value)* parseFloat(input.getAttribute('data-price'));
+                inputsGreaterThanZero.push(amount);
+                console.log(input.value, input.getAttribute('data-price'));
+                total += amount;
+            }
+        }
+
+        // Ahora 'inputsGreaterThanZero' contiene solo los inputs que cumplen la condición
+        console.log(inputsGreaterThanZero, total);
+        totalAmountDisplay.textContent = total;
+    };
+
+
+    const rendProd=data=>{  items.innerHTML='';
+        data.forEach(producto=>{
+            //templateDetail.querySelector('.stk').textContent = producto.prid;//
+            //templateProd.querySelector('.img-box').style.backgroundImage = `url(${producto.img})`;
+            templateProd.querySelector('.description').textContent = producto.name;        
+            templateProd.querySelector('.price').textContent = producto.price;
+            templateProd.querySelector('.quantity-input').setAttribute('data-price', producto.price);
+            const clone = templateProd.cloneNode(true);
+            //clone.querySelector('.det-row').setAttribute('prid',producto.prid);
+            fragment.appendChild(clone);
+        });    items.appendChild(fragment);
+    }
+    const fetchData = async()=>{
+        try{
+            const res=await fetch('products.js');
+            const data=await res.json(); 
+            rendProd(data);
+        }
+        catch (error){console.log(error);  } 
+    }
+
+    fetchData();
+
 
     // Función para recalcular el total de todos los inputs
     const updateTotalQuantity = () => {
@@ -46,4 +96,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Inicializa el total al cargar la página
     updateTotalQuantity();
-});
+//});
